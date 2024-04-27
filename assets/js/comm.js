618859b81bac4c1dcc07fc57c6af420da3ffc25c
minji0164 <min016425@gmail.com>
2024-04-27 19:03:33 +0900

    세팅 및 인트로 제작

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,3 +1,33 @@
-var swiper = new Swiper(".about-slide", {
-  slidesPerView: 1,
+document.addEventListener("DOMContentLoaded", function () {
+  function drawText(word) {
+    word = word || "WELCOME!";
+    var wordArray = word.split("");
+    var innerTextElement = document.getElementById("inner-text");
+    innerTextElement.innerHTML = "";
+    innerTextElement.classList.remove("done-animating");
+    wordArray.forEach(function (letter, index) {
+      var span = document.createElement("span");
+      span.textContent = letter;
+      span.className = "letter animate";
+      span.style["animation-delay"] = 150 * index + "ms";
+      innerTextElement.appendChild(span);
+
+      if (index === wordArray.length - 1) {
+        span.addEventListener("animationend", function () {
+          innerTextElement.classList.add("done-animating");
+        });
+      }
+    });
+  }
+
+  document
+    .getElementById("custom-text-form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      drawText(document.getElementById("custom-text").value);
+    });
+
+  drawText();
 });
