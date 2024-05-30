975cd0861c978c79905e251830707b7eb167653e
minji0164 <min016425@gmail.com>
2024-05-30 21:00:11 +0900

    back to top btn 생성

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,4 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
+  (function () {
+    "use strict";
+
+    function trackScroll() {
+      var scrolled = window.pageYOffset;
+      var coords = document.documentElement.clientHeight;
+
+      if (scrolled > coords) {
+        goTopBtn.classList.add("back_to_top-show");
+      }
+      if (scrolled < coords) {
+        goTopBtn.classList.remove("back_to_top-show");
+      }
+    }
+
+    function backToTop() {
+      if (window.pageYOffset > 0) {
+        window.scrollBy(0, -60);
+        setTimeout(backToTop, 10);
+      }
+    }
+
+    var goTopBtn = document.querySelector(".back_to_top");
+
+    window.addEventListener("scroll", trackScroll);
+    goTopBtn.addEventListener("click", backToTop);
+  })();
+
   function drawText(word) {
     word = word || "WELCOME!";
     var wordArray = word.split("");
